4419519b2b8d777132be9d62b8339c76de614fdc
Tanishq-13 <tanishqgupta326999@gmail.com>
2026-05-15 02:42:08 +0530

    fixed auth

--- src/main/java/com/iiituna/placementportal/controller/AuthController.java
+++ src/main/java/com/iiituna/placementportal/controller/AuthController.java
@@ -1,25 +1,25 @@
-package com.iiituna.placementportal.controller;
-
-import com.iiituna.placementportal.dto.LoginRequestDto;
-import com.iiituna.placementportal.dto.SignupRequestDto;
-import com.iiituna.placementportal.service.AuthService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/auth")
-@RequiredArgsConstructor
-public class AuthController {
-
-    private final AuthService authService;
-
-    @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequestDto dto) {
-        return authService.signup(dto);
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDto dto) {
-        return authService.login(dto);
-    }
-}
+//package com.iiituna.placementportal.controller;
+//
+//import com.iiituna.placementportal.dto.LoginRequestDto;
+//import com.iiituna.placementportal.dto.SignupRequestDto;
+//import com.iiituna.placementportal.service.AuthService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/auth")
+//@RequiredArgsConstructor
+//public class AuthController {
+//
+//    private final AuthService authService;
+//
+//    @PostMapping("/signup")
+//    public String signup(@RequestBody SignupRequestDto dto) {
+//        return authService.signup(dto);
+//    }
+//
+//    @PostMapping("/login")
+//    public String login(@RequestBody LoginRequestDto dto) {
+//        return authService.login(dto);
+//    }
+//}
